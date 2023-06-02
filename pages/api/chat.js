56bfc311cff9385967588b983632aa68e7b34fc5
B <philoman@gmail.com>
2023-06-02 14:52:40 +0800

    tset

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -24,7 +24,7 @@ export default async function (req, res) {
   if (id.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Invalid id",
+        message: "Invalid id.",
       },
     });
     return;
